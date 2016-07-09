6fcb36461e21eb91b5a4a8e1d47f9be255a180c7
harrytina <nwnulijing@nwnu.edu.cn>
2016-07-09 14:49:23 +0800

    first commit

---
+++ app/src/main/java/cn/edu/example/nwnu/myapplication/myActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.example.nwnu.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zzp on 2016/7/9.
+ */
+public class myActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+
+
+    }
+}
